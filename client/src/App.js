f7c23728771e5bfc6923ddec0ab93224e73ee9cd
shrutipanjwani <67150258+shrutipanjwani@users.noreply.github.com>
2020-12-05 22:55:22 +0530

    adding redux calls

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch> 
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav className="navbar bg-dark">
+	      <h1>
+	        <Link to="/">
+	        	<i className="fas fa-code"> Developer Cafe</i> 
+	        </Link>
+	      </h1>
+	      <ul>
+	        <li>
+	        	<a href="#">
+	        		Developers
+	        	</a>
+	        </li>
+	        <li>
+	        	<Link to="/register">
+	        		Register
+	        	</Link>
+	        </li>
+	        <li>
+	        	<Link to="/login">
+	        		Login
+	        	</Link>
+	        </li>
+	      </ul>
+	    </nav>
+	);
+}
+
+export default Navbar;
